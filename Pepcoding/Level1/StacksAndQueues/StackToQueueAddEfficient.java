22ba26620cb9c15bec24424ec3a4fcf6fc5810a8
Daksh Khetan <2012daksh@gmail.com>
2021-01-08 20:33:11 +0530

    stacks-and-queues pt-10

---
+++ Pepcoding/Level1/StacksAndQueues/StackToQueueAddEfficient.java
@@ -0,0 +1,117 @@
+/*
+
+Sample Input
+add 10
+add 20
+add 30
+remove
+remove
+add 40
+add 50
+peek
+remove
+peek
+remove
+remove
+remove
+quit
+
+Sample Output
+10
+20
+30
+30
+40
+40
+50
+Queue underflow
+
+*/
+
+import java.util.*;
+
+class StackToQueue {
+  Stack<Integer> stack;
+  Stack<Integer> helperStack;
+
+  public StackToQueue() {
+    stack = new Stack<>();
+    helperStack = new Stack<>();
+  }
+
+  int size() {
+    return stack.size();
+  }
+
+  void add(int val) {
+    stack.push(val);
+  }
+
+  int remove() {
+    if (stack.isEmpty()) {
+      System.out.println("Queue underflow");
+      return -1;
+    } else {
+      while (stack.size() > 1) {
+        helperStack.push(stack.pop());
+      }
+
+      int val = stack.pop();
+
+      while (!helperStack.isEmpty()) {
+        stack.push(helperStack.pop());
+      }
+
+      return val;
+    }
+  }
+
+  int peek() {
+    if (stack.isEmpty()) {
+      System.out.println("Queue underflow");
+      return -1;
+    } else {
+      while (stack.size() > 1) {
+        helperStack.push(stack.pop());
+      }
+
+      int val = stack.peek();
+
+      while (!helperStack.isEmpty()) {
+        stack.push(helperStack.pop());
+      }
+
+      return val;
+    }
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    StackToQueue queue = new StackToQueue();
+
+    String str = s.nextLine();
+    while (!str.equals("quit")) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        queue.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = queue.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = queue.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(queue.size());
+      }
+      str = s.nextLine();
+    }
+  }
+
+}
