417aadc5c239d18e9b4f844ae45e6489197fc761
Daksh Khetan <2012daksh@gmail.com>
2021-01-24 21:45:16 +0530

    generic-trees pt-1

---
+++ Pepcoding/Level1/GenericTrees/MaximumInAGenericTree.java
@@ -0,0 +1,94 @@
+/*
+
+Sample Input
+12
+10 20 -1 30 50 -1 60 -1 -1 40 -1 -1
+
+Sample Output
+60
+
+*/
+
+import java.util.*;
+
+class TreeNode {
+  int data;
+  ArrayList<TreeNode> children;
+
+  TreeNode(int data) {
+    this.data = data;
+    children = new ArrayList<>();
+  }
+}
+
+public class Main {
+
+  public static void display(TreeNode root) {
+    String str = root.data + " -> ";
+    for (TreeNode child : root.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (TreeNode child : root.children) {
+      display(child);
+    }
+  }
+
+  public static TreeNode construct(int[] arr) {
+    TreeNode root = null;
+    Stack<TreeNode> st = new Stack<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        TreeNode node = new TreeNode(arr[i]);
+
+        if (st.size() > 0) {
+          st.peek().children.add(node);
+        } else {
+          root = node;
+        }
+
+        st.push(node);
+      }
+    }
+
+    return root;
+  }
+
+  public static int maxNodeValue(TreeNode root) {
+    if (root == null) {
+      return Integer.MIN_VALUE;
+    }
+
+    int max = root.data;
+
+    for (TreeNode child : root.children) {
+      int childAns = maxNodeValue(child);
+      max = Math.max(childAns, max);
+    }
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    int n = s.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = s.nextInt();
+    }
+
+    TreeNode root = construct(arr);
+
+    int result = maxNodeValue(root);
+    System.out.println(result);
+
+    // display(root);
+  }
+
+}
---
+++ Pepcoding/Level1/GenericTrees/SizeOfGenericTree.java
@@ -0,0 +1,93 @@
+/*
+
+Sample Input
+12
+10 20 -1 30 50 -1 60 -1 -1 40 -1 -1
+
+Sample Output
+6
+
+*/
+
+import java.util.*;
+
+class TreeNode {
+  int data;
+  ArrayList<TreeNode> children;
+
+  TreeNode(int data) {
+    this.data = data;
+    children = new ArrayList<>();
+  }
+}
+
+public class Main {
+
+  public static void display(TreeNode root) {
+    String str = root.data + " -> ";
+    for (TreeNode child : root.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (TreeNode child : root.children) {
+      display(child);
+    }
+  }
+
+  public static TreeNode construct(int[] arr) {
+    TreeNode root = null;
+    Stack<TreeNode> st = new Stack<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        TreeNode node = new TreeNode(arr[i]);
+
+        if (st.size() > 0) {
+          st.peek().children.add(node);
+        } else {
+          root = node;
+        }
+
+        st.push(node);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int count = 1; // root
+
+    for (TreeNode child : root.children) {
+      count += size(child);
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    int n = s.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = s.nextInt();
+    }
+
+    TreeNode root = construct(arr);
+
+    int result = size(root);
+    System.out.println(result);
+
+    // display(root);
+  }
+
+}
